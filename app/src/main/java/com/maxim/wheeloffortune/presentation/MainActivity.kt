bda63871d6d112b1578bb40f4f26281c1293accf
Diryfort <dorhungmi2@gmail.com>
2023-11-14 09:19:34 +0300

    added night theme

--- app/src/main/java/com/maxim/wheeloffortune/presentation/MainActivity.kt
+++ app/src/main/java/com/maxim/wheeloffortune/presentation/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.maxim.wheeloffortune.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.UiModeManager
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.maxim.wheeloffortune.R
 import com.maxim.wheeloffortune.presentation.main.MainFragment
@@ -10,9 +15,21 @@ class MainActivity : AppCompatActivity(), FragmentManager {
     private var backStackHomeButtonList = mutableListOf<Boolean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            (getSystemService(Context.UI_MODE_SERVICE) as UiModeManager).setApplicationNightMode(
+                UiModeManager.MODE_NIGHT_AUTO
+            )
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null)
             replaceFragment(MainFragment(), false, false)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        recreate()
     }
 
     fun setActionBarTitle(title: String) {
